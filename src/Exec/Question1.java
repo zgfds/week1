61863dc3b1d2ce09f78e642f44db14c7e5e714f4
minseok <zjatjrtjr777@gmail.co>
2025-09-24 14:40:01 +0900

    첫 번째 문제완료

--- src/Exec/Question1.java
+++ src/Exec/Question1.java
@@ -1,7 +1,17 @@
 package Exec;
 
+import java.util.Scanner;
+
 public class Question1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정수를 입력하세요: ");
+        int a = sc.nextInt();
 
+        if (a % 2== 0) {
+            System.out.println("짝수입니다");
+        }else {
+            System.out.println("홀수입니다");
+        }
     }
 }
